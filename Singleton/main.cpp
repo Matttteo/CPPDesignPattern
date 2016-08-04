7d1ad31cbebe0043d23f97010332f2ca976b5776
Matttteo <baiyunhan.cs@gmail.com>
2016-08-04 09:19:52 +0800

    Remove the now ignored directory

--- Singleton/main.cpp
+++
@@ -1,9 +0,0 @@
-#include "Logging.h"
-
-int main() {
-	Logging* log1 = Logging::getInstance();
-	Logging* log2 = Logging::getInstance();
-	log1->loggingInformation();
-	log2->loggingInformation();
-	return 0;
-}
